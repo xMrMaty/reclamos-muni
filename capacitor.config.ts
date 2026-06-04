4e4595665828a874b2dce290fae2938e22725bdd
Joako-C <142269210+Joako-C@users.noreply.github.com>
2026-06-03 21:41:46 -0400

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'reclamos-muni',
-  webDir: 'dist'
-};
-
-export default config;
